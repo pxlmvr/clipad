4da17124db515a197dfc21f8481281788b37cd3d
pxlmvr <matteo.losi@casavo.com>
2023-09-05 10:20:23 +0200

    feat: test main db functionalities

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -18,7 +18,7 @@ beforeEach(() => {
   saveDB.mockClear()
 })
 
-describe('insertDB', () => {
+describe('db helpers', () => {
   test('newNote inserts data and returns it', async () => {
     const note = {
       content: 'test note',
@@ -26,9 +26,48 @@ describe('insertDB', () => {
       id: Date.now(),
     }
 
-    insertDB.mockResolvedValue(newNote)
+    insertDB.mockResolvedValue(note)
 
     const result = await newNote(note.content, note.tags)
-    expect(result).toEqual(note)
+    expect(result.content).toEqual(note.content)
+    expect(result.tags).toEqual(note.tags)
+  })
+
+  test('newNote can create a note without tags', async () => {
+    const note = {
+      content: 'test note',
+      id: Date.now(),
+    }
+
+    insertDB.mockResolvedValue(note)
+
+    const result = await newNote(note.content, [])
+    expect(result.tags).toEqual([])
+  })
+
+  test('getAllNotes returns all notes', async () => {
+    const db = {
+      notes: ['note1', 'note2', 'note3'],
+    }
+
+    getDB.mockResolvedValue(db)
+
+    const result = await getAllNotes()
+    expect(result).toEqual(db.notes)
+  })
+
+  test('removeNote does nothing if id is not found', async () => {
+    // populate the DB
+    const notes = [
+      { id: 1, content: 'note 1' },
+      { id: 2, content: 'note 2' },
+      { id: 3, content: 'note 3' },
+    ]
+
+    saveDB.mockResolvedValue(notes)
+
+    const idToRemove = 4
+    const result = await removeNote(idToRemove)
+    expect(result).toBeUndefined()
   })
 })
